56c358ba205c9572babe0958ea55b3a045bac5d3
Jeremy Campbell <jeremyc61298@gmail.com>
2018-10-18 20:36:38 -0500

    Working on initilize functions.

--- Hi-Q/hiq.cpp
+++ Hi-Q/hiq.cpp
@@ -6,22 +6,65 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <map>
 
 using std::cerr;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
 using std::string;
+using std::vector;
+using std::map;
 
 class HiQ
 {
 public:
+	HiQ();
 	void play(char vacantHole);
 	void printResults(char newVacantHole);
 
 private:
+	const int NUMHOLES = 15;
 	string solution;
+	// TODO: Might go back to the vector<vector> here, as discovering 
+	// that an item is non-existent is possibly less efficient with a map
+	map<char, map<char, char>> between;
+	// Because of the unorthodox indexing for this 
+	// game board (A - Y without O represent 10-33) 
+	// a map is used to access the between matrix. 
+	map<char, int> index;
+
+	void initializeIndex();
+	void initializeBetween();
 };
+
+HiQ::HiQ() 
+{
+	initializeIndex();
+	initializeBetween();
+}
+
+void HiQ::initializeIndex()
+{
+	char letter = 'A';
+	for (int i = 10; i <= 33; i++)
+	{
+		if (letter == 'O')
+			++letter;
+		
+		index[letter++] = i;
+	}
+}
+
+void HiQ::initializeBetween()
+{
+	// At between[i][j], the value for the hole "between" i and j is found. 
+	// If you cannot jump from i to j, then the value is 0
+	
+
+
+}
 
 void HiQ::play(char vacantHole)
 {
@@ -52,7 +95,7 @@ int main()
 
 	while (vacantHole != '0')
 	{
-		game.playHiQ(vacantHole);
+		game.play(vacantHole);
 		fin >> vacantHole;
 		game.printResults(vacantHole);	
 	}	
