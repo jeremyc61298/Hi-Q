de73fbe7baa58fa2ef3e48cec0b4f19dd266730b
Jeremy Campbell <jeremyc61298@gmail.com>
2018-10-19 23:34:28 -0500

    Completes the tests cases correctly, but not within the time constraints.

--- Hi-Q/hiq.cpp
+++ Hi-Q/hiq.cpp
@@ -1,13 +1,17 @@
 // ---------------------------------------------------
 // hiq.cpp
 // Jeremy Campbell
-// Plays the game Hi-Q 
+// Solves the game HiQ using a Depth-First Search
+// Logic is encapsulated in the HiQ class.
 // ---------------------------------------------------
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <map>
+#include <bitset>
+#include <stack>
+#include <set>
 
 using std::cerr;
 using std::endl;
@@ -16,38 +20,60 @@ using std::ofstream;
 using std::string;
 using std::vector;
 using std::map;
+using std::bitset;
+using std::stack;
+using std::set;
+
+#define NUMHOLES 33
 
 class HiQ
 {
 public:
 	HiQ();
 	void play(char vacantHole);
-	void printResults(char newVacantHole);
+	void printResults(ofstream &fout, char vacantHole);
 
 private:
-	const int NUMHOLES = 15;
-	string solution;
+	/* ---- VARIABLES ---- */
 	vector<vector<char>> between;
-	// Because of the unorthodox indexing for this 
-	// game board (A - Y without O represent 10-33) 
-	// a map is used to access the between matrix. 
 	map<char, short> index;
+	map<short, char> rindex;
+	typedef bitset<NUMHOLES + 1> BoardState;
+	BoardState initialBoardState;
+	BoardState solutionBoardState;
+	struct jumpType { char from; char to; };
+	stack<jumpType> jumpStack;
+	set< unsigned long long > markedSet;
 
-	void initializeIndex();
+	/* ---- MEHTODS ---- */
+	void initializeIndices();
 	void setBetween(char first, char last, char mid);
 	void initializeBetween();
+	void initializeBoardStates(char initialHole);
+	bool findSolution(BoardState currentBoardState);
+	void mark(BoardState currentBoardState);
+	bool marked(BoardState currentBoardState);
+	BoardState findNewBoardState(jumpType jump, BoardState currentBoardState);
+	bool validJump(jumpType jump, BoardState currentBoardState);
+
+	template <typename T>
+	void print2dVector(string name, const vector<vector<T>> &v);
 };
+
 
 HiQ::HiQ() 
 {
-	initializeIndex();
+	initializeIndices();
 	initializeBetween();
 }
 
-void HiQ::initializeIndex()
+// Because of the unorthodox indexing for this 
+// game board (1-9, A - Y without O represent 10-33) 
+// a map is used to access the between matrix. 
+void HiQ::initializeIndices()
 {
 	char letter;
-	for (int i = 10; i <= 33; i++)
+	for (int i = 0; i <= 33; i++)
 	{
 		// The index is the actual number (1-9)
 		if (i < 10)
@@ -63,13 +89,20 @@ void HiQ::initializeIndex()
 		
 		index[letter++] = i;
 	}
+
+	// rindex is the reverse of index, with the values of index
+	// as the keys of rindex.
+	for (auto entry : index)
+	{
+		rindex[entry.second] = entry.first;
+	}
 }
 
 void HiQ::initializeBetween()
 {
 	// At between[i][j], the value for the hole "between" i and j is found. 
 	// If you cannot jump from i to j, then the value is 0
-	between.resize(NUMHOLES + 1, vector<char>(NUMHOLES + 1, 0));
+	between.resize(NUMHOLES + 1, vector<char>(NUMHOLES + 1, '0'));
 
 	setBetween('1', '9', '4');
 	setBetween('1', '3', '2');
@@ -109,6 +142,8 @@ void HiQ::initializeBetween()
 	setBetween('Q', 'Y', 'V');
 	setBetween('T', 'V', 'U');
 	setBetween('W', 'Y', 'X');
+
+	//print2dVector("Between", between);
 }
 
 // At between[i][j], the value for the hole "between" i and j is found. 
@@ -121,13 +156,133 @@ void HiQ::setBetween(char first, char last, char mid)
 
 void HiQ::play(char vacantHole)
 {
+	initializeBoardStates(vacantHole);
+	markedSet.clear(); // Make sure the set is ready for another solution
+	findSolution(initialBoardState);
+}
 
+void HiQ::initializeBoardStates(char initialHole)
+{
+	initialBoardState.set();
+	initialBoardState.flip(0);
+	initialBoardState.flip(index[initialHole]);
+
+	solutionBoardState.reset();
+	solutionBoardState.flip(index[initialHole]);
+}
+
+// This function was given by Dr. Baber in class.
+// I have modified it slightly to fit with my program structure.
+// His function was not a part of a class
+bool HiQ::findSolution(BoardState currentBoardState)
+{
+	bool solutionFound = false;
+	if (currentBoardState == solutionBoardState) { solutionFound = true;  }
+	else
+	{
+		mark(currentBoardState);
+		for (short from = 1; !solutionFound && from <= NUMHOLES; from++)
+		{
+			for (short to = 1; !solutionFound && to <= NUMHOLES; to++)
+			{
+				// Must translate the jump from shorts to chars
+				jumpType jump = { rindex.at(from), rindex.at(to) };
+				BoardState newBoardState = findNewBoardState(jump, currentBoardState);
+				if (!marked(newBoardState))
+				{
+					solutionFound = findSolution(newBoardState);
+					if (solutionFound)
+						jumpStack.push(jump);
+				}
+			}
+		}
+	}
+
+	return solutionFound;
+}
+
+// Given by Dr. Baber in class
+void HiQ::mark(BoardState currentBoardState)
+{
+	markedSet.insert(currentBoardState.to_ullong());
+}
+
+bool HiQ::marked(BoardState currentBoardState)
+{
+	bool found = false;
+	found = markedSet.count(currentBoardState.to_ullong()) == 1;
+	return found;
+}
+
+// Given by Dr. Baber in class, modified slightly (different names)
+HiQ::BoardState HiQ::findNewBoardState(jumpType jump, BoardState currentBoardState)
+{
+	BoardState newBoardState = currentBoardState;
+
+	if (validJump(jump, currentBoardState))
+	{
+		// Change the board to represent the valid jump has occured
+		newBoardState.flip(index.at(jump.from));
+		newBoardState.flip(index.at(jump.to));
+		newBoardState.flip(index.at(between[index.at(jump.from)][index.at(jump.to)]));
+	}
+
+	return newBoardState;
+}
+
+// Given by Dr. Baber, modified slightly (using my index map)
+bool HiQ::validJump(jumpType jump, BoardState currentBoardState)
+{
+	// This function returns true if
+	// (1) there is a pegin the 'jump.from' hole AND
+	// (2) there is no peg in the 'jump.to' hole AND
+	// (3) there is a peg in the hole between 'jump.from' and 'jump.to'
+	// otherwise, the function returns false
+	bool valid = false;
+	if (currentBoardState.test(index.at(jump.from)) &&
+		!currentBoardState.test(index.at(jump.to)) &&
+		currentBoardState.test(index.at(between[index.at(jump.from)][index.at(jump.to)])))
+	{
+		valid = true;
+	}
+
+	return valid;
+}
+
+// Debug function
+template <typename T>
+void HiQ::print2dVector(string name, const vector<vector<T>> &v)
+{
+	cerr << name << ": " << endl;
+	cerr << "---------------------------------" << endl;
+	for (vector<T> row : v)
+	{
+		for (T col : row)
+		{
+			cerr << col << " ";
+		}
+		cerr << endl;
+	}
+	cerr << "---------------------------------" << endl;
 }
 
 // Need the new vacantHole to know if the endl should be printed
-void HiQ::printResults(char newVacantHole)
+void HiQ::printResults(ofstream &fout, char vacantHole)
 {
+	if (jumpStack.empty())
+		fout << "No solution for hole at " << vacantHole;
+	else
+	{
+		// There is a solution
+		while (!jumpStack.empty())
+		{
+			jumpType jump = jumpStack.top(); jumpStack.pop();
+			fout << jump.from << "->" << jump.to;
 
+			if (!jumpStack.empty())
+				fout << " ";
+		}
+	}
 }
 
 int main()
@@ -149,7 +304,10 @@ int main()
 	while (vacantHole != '0')
 	{
 		game.play(vacantHole);
+		game.printResults(fout, vacantHole);	
+		cerr << "Solved at " << vacantHole << endl;
 		fin >> vacantHole;
-		game.printResults(vacantHole);	
+		if (vacantHole != '0')
+			fout << endl;
 	}	
 }
